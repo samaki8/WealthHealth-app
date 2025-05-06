6c42fcf3f08b166b77eaafca675c04bda8ec46cc
Samira <samira08@gmail.com>
2025-05-06 14:57:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,7 @@ import { useEffect } from 'react'
 
 
 function App() {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const script = document.createElement('script');
-      script.src = "/headingsmap.js"; // Assurez-vous que le chemin est correct
-      script.async = true;
-      document.body.appendChild(script);
 
-      // Nettoyage du script lors du démontage
-      return () => {
-        document.body.removeChild(script);
-      };
-    }
-  }, []);
 
 
 
